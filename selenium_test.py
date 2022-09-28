a529d4804d4b0a85de44d3a19450cd59a0b5e729
suregoth <suregoth@wp.pl>
2022-09-28 20:34:51 +0200

    new tests added

--- selenium_test.py
+++ selenium_test.py
@@ -5,11 +5,12 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.service import Service
 import time
+from selenium.webdriver.chrome.options import Options
 
-
-
-s = Service('C:\sel\chromedriver.exe')
-driver = webdriver.Chrome(service=s)
+options = Options()
+options.add_experimental_option('excludeSwitches', ['enable-logging'])
+PATH=Service('C:\sel\chromedriver.exe')
+driver = webdriver.Chrome(service=PATH, options=options)
 
 
 driver.get("https://www.google.com/")
@@ -21,17 +22,22 @@ consent_button.click()
 
 
 search = driver.find_element(By.CSS_SELECTOR, "input.gLFyf")
-search.send_keys("ile nóg ma padalec")
+search.send_keys("knitting patterns")
 search.send_keys(Keys.RETURN)
 
-# try:
-#     main = WebDriverWait(driver, 10).until(
-#         EC.presence_of_element_located((By.ID, "main"))
-#     )
-# finally:
-#     driver.quit()
+try:
+    main = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.ID, "main"))
+    )
+    articles = main.find_elements(By.TAG_NAME, "div")
+    for art in articles:
+        header = art.find_element(By.CSS_SELECTOR, ".LC20lb.MBeuO.DKV0Md")
+        print(header.text)
 
-# print(main.text)
+finally:
+    driver.quit()
 
-time.sleep(20)
-driver.quit()
+
+
+# time.sleep(5)
+# driver.quit()
